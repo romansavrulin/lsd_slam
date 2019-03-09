7aaa84552b8decad12061df600d2625d5d7e597b
Aaron Marburg <amarburg@apl.washington.edu>
2019-03-08 19:57:32 -0800

    Stubbed in Frame unit test.

---
+++ test/unit/test_Frame.cpp
@@ -0,0 +1,24 @@
+
+#include <gtest/gtest.h>
+
+#include "libvideoio/Camera.h"
+#include "libvideoio/ImageSize.h"
+
+#include "DataStructures/Frame.h"
+
+TEST( Frame, constructor )
+{
+  const libvideoio::Camera cam(1000,1000,320,240);
+  const libvideoio::ImageSize sz(640,480);
+  const int id = 123;
+  const double timestamp = 1.5;
+
+  const unsigned char *img = new unsigned char[ sz.width + sz.height ];
+
+  {
+    lsd_slam::Frame frame( id, cam, sz, timestamp, img );
+
+
+  }
+  delete[] img;
+}
